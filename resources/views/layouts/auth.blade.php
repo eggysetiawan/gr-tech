1232ebd27024ccfd57ae49b6693e97f0114a6f95
Rahmat Setiawan <setiawaneggy@gmail.com>
2021-07-28 20:31:58 +0700

    fix: title on login

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ $title ?? 'Login | GR Tech' }}</title>
+    <title>{{ $title ?? 'Login | ' . config('app.name', 'GR Tech') }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
